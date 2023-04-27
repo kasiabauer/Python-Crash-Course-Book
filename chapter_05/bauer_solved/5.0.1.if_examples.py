e4e66a03cb4a979ed4594af9322390560f00c0fa
kasiabauer <bauer.kasia@gmail.com>
2023-04-27 16:39:54 +0200

    chapter 5 - exercises 5.2, 5.3

--- chapter_05/bauer_solved/5.0.1.if_examples.py
+++ chapter_05/bauer_solved/5.0.1.if_examples.py
@@ -1,3 +1,4 @@
+# tickets version 1
 age = 17
 if age >= 18:
     print('Możesz wziąć udział w głosowaniu')
@@ -6,6 +7,7 @@ else:
     print('Przykro nam, jesteś zbyt młody aby głosować.')
     print('Możesz się zarejestrować po ukończeniu 18 lat!')
 
+# tickets version 2
 age = 13
 if age < 4:
     print('Cena biletu wstępu wynosi: 0 zł.')
@@ -13,3 +15,55 @@ elif age < 18:
     print('Cena biletu wstępu wynosi: 25 zł.')
 else:
     print('Cena biletu wstępu wynosi: 40 zł.')
+
+# tickets version 3
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f'Cena biletu wstępu wynosi {price} zł.')
+
+# tickets version 4
+age = 65
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+
+print(f'Cena biletu wstępu wynosi {price} zł.')
+
+# tickets version 5
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    prince = 20
+
+print(f'Cena biletu wstępu wynosi {price} zł.')
+
+
+# pizza version 1
+requested_toppings = ['pieczarki', 'podwójny ser']
+
+if 'pieczarki' in requested_toppings:
+    print('Dodaję pieczarki')
+if 'pepperoni' in requested_toppings:
+    print('Dodaję pepperoni')
+if 'podwójny ser' in requested_toppings:
+    print('Dodaję podwójny ser')
+
+print(f'\nTwoja pizza jest już gotowa')
---
+++ chapter_05/bauer_solved/5.3.aliens.py
@@ -0,0 +1,19 @@
+# alien version 1
+alien_color = 'zielony'
+
+if alien_color == 'zielony':
+    points = 5
+print(f'Gracz uzyskał {points} punktów.')
+
+# alien version 2
+if alien_color == 'zielony':
+    points = 5
+else:
+    points = 0
+print(f'Gracz uzyskał {points} punktów.')
+
+# alien version 3 - failed
+if alien_color == 'czerwony':
+    alien_points = 10
+print(f'Gracz uzyskał {alien_points} punktów.')
+
