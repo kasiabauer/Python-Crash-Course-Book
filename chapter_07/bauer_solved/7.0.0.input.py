853b3b0f98d598c9d06c32e8d7ca17206dcaf6b4
kasiabauer <bauer.kasia@gmail.com>
2023-05-04 08:54:19 +0200

    chapter 7: - added lesson notes 7.0.0

---
+++ chapter_07/bauer_solved/7.0.0.input.py
@@ -0,0 +1,18 @@
+print('Example #1')
+message = input("Powiedź mi coś o sobie, a wyświetlę to na ekranie: ")
+print(message)
+
+name = input('Podaj swoje imię: ')
+print(f'Twoje imię: {name}')
+
+print('Example #2')
+prompt = 'Jeżeli powiesz nam, kim jesteś, spersonalizujemy wyświetlany komunikat.'
+prompt +='\nJak masz na imię?: '
+
+name = input(prompt)
+print(f"\nWitaj, {name}!")
+
+print('Example #3')
+
+age = input('Ile masz lat? ')
+print(age)
